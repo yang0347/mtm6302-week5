0291127605059aec176c92e81e0404c7517b7e93
yuwei yang <yang0347@algonquinlive.com>
2023-06-09 12:46:25 -0400

    s

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello from the Script JS")
+//creating an array using [] and comma separated list of valus 
+//index of an array starts with 0
+const myColor = ["red", "green", "blue", "white", "black", "tomato"]
+
+//access the element with id and save in a variable
+const messageList = document.getElementById("colorMessages")
+
+messageList.children[0].innerHTML += myColor[3]
+
+myColor[4] = "cyan"
+
+messageList.children[1].innerHTML += myColor[4]
