27a3fb035697c7d47ecf92ff0cf54ee30ac2eba5
yuwei yang <yang0347@algonquinlive.com>
2023-06-09 13:53:00 -0400

    s

--- js/script.js
+++ js/script.js
@@ -10,8 +10,33 @@ messageList.children[0].innerHTML += myColors[3]
 myColors[4] = "cyan"
 
 messageList.children[1].innerHTML += myColors[4]
-//messageList.children[]
+//messageList.children[]是定具体位置的 
 
 myColors.push("darksalmon")
 
 messageList.children[2].innerHTML += myColors
+
+myColors.pop()
+messageList.children[3].innerHTML += myColors
+
+
+myColors.shift()
+messageList.children[4].innerHTML += myColors
+
+myColors.unshift("hotpink")
+messageList.children[5].innerHTML += myColors
+
+//add a new value at index 2, using splice() start index 2, delete 0, new value "royalblue"
+myColors.splice(2,0,"royalblue")
+messageList.children[6].innerHTML += myColors
+
+//sorting array using sort() method, default for all lowercase text is ascending alphabetial
+myColors.sort()
+messageList.children[7].innerHTML += myColors
+
+//connecting arrys using concat
+const darkColors = ["darkseagreen", "darkgreen", "darkred"]
+
+const allColors = myColors.concat(darkColors)
+messageList.children[8].innerHTML += allColors.join("-")
+
