6c60ff1cb292ac1086bb0a368a0c4f43b91523da
yuwei yang <yang0347@algonquinlive.com>
2023-06-09 12:49:25 -0400

    s

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 //creating an array using [] and comma separated list of valus 
 //index of an array starts with 0
-const myColor = ["red", "green", "blue", "white", "black", "tomato"]
+const myColors = ["red", "green", "blue", "white", "black", "tomato"]
 
 //access the element with id and save in a variable
 const messageList = document.getElementById("colorMessages")
 
-messageList.children[0].innerHTML += myColor[3]
+messageList.children[0].innerHTML += myColors[3]
 
-myColor[4] = "cyan"
+myColors[4] = "cyan"
 
-messageList.children[1].innerHTML += myColor[4]
+messageList.children[1].innerHTML += myColors[4]
+//messageList.children[]
+
+myColors.push("darksalmon")
+
+messageList.children[2].innerHTML += myColors
